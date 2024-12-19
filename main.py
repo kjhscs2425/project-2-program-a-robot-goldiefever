5a9034f6e7ad85693d59a5a3cd4476719c2482e0
goldiefever <mgoldhaberfiebert2028@kehillahstudent.org>
2024-12-19 10:51:13 -0800

    done! (finally)

--- main.py
+++ main.py
@@ -6,45 +6,65 @@ from time import *
 
 # left, right = robot.sonars()
 
+degree = 0
+
 def spin(spin_direction, spin_duration):
+    global degree
     if spin_direction == "r" or spin_direction == "R" or spin_direction == "right" or spin_direction == "Right":
         robot.motors(-1, 1, spin_duration)
+        degree = (degree-(spin_duration*58.8))%360
+        print(degree)
     if spin_direction == "l" or spin_direction == "L" or spin_direction == "left" or spin_direction == "Left":
         robot.motors(1,-1, spin_duration)
+        degree = (degree+(spin_duration*58.8))%360
+        print(degree)
 
 def turn90():
+    global degree
     turn_direction = input("do you want to turn left (L) or right (R)? ")
     if turn_direction == "r" or turn_direction == "R" or turn_direction == "right" or turn_direction == "Right":
         robot.motors(-1, 1, (90/58.8))
+        degree = (degree-90)%360
     if turn_direction == "l" or turn_direction == "L:" or turn_direction == "left" or turn_direction == "Left":
         robot.motors(1, -1, (90/58.8))
+        degree = (degree+90)%360
 
 def dance():
-    robot.motors(-1,1,1)
-    robot.motors(1,-1,2)
-    robot.motors(-1,1,1)
-    robot.motors(1,1,1)
-    robot.motors(-1,-1,2)
-    robot.motors(1,1,1)
-    robot.motors(1,-1,1)
-    robot.motors(-1,1,2)
-    robot.motors(1,-1,1)
-    robot.motors(-1,-1,1)
-    robot.motors(1,1,2)
-    robot.motors(-1,-1,1)
-    robot.motors(-1,1,1)
-    robot.motors(1,-1,2)
-    robot.motors(-1,1,1)
+    global degree
+    dancing = float(input("how many dance loops do you want me to do (one loop is 10 seconds)? "))
+    while dancing > 0:
+        robot.motors(-1,1,0.5)
+        robot.motors(1,-1,1)
+        robot.motors(-1,1,0.5)
+        robot.motors(1,1,0.5)
+        robot.motors(-1,-1,1)
+        robot.motors(1,1,0.5)
+        robot.motors(1,-1,0.5)
+        robot.motors(-1,1,1)
+        robot.motors(1,-1,0.5)
+        robot.motors(-1,-1,0.5)
+        robot.motors(1,1,1)
+        robot.motors(-1,-1,0.5)
+        robot.motors(-1,1,0.5)
+        robot.motors(1,-1,1)
+        robot.motors(-1,1,0.5)
+        dancing-=1
+        degree = degree%360
 
 def drive():
+    global degree
     drive_duration = float(input("how long do you want to drive for? "))
     l = 1
     r = 1
     def forward_drive(drive_duration):
+        global degree
+        #print(drive_duration)
         remainder = drive_duration
         left,right=robot.sonars()
         if left<100 or right<100:
-            robot.motors(-r, l, 3.04)
+            robot.motors(-r, l, 3.06)
+            remainder = remainder-0.1
+            degree = (degree+180)%360
             if remainder <0.1:
                 return
             forward_drive(remainder)
@@ -59,16 +79,31 @@ def drive():
             
     # robot.motors(r,l,drive_duration)
 
-# def center():
-#     no=2
-# # def backup():
-# #     robot.motors(1,1,30)
+def center():
+    global degree
+    print(degree)
+    if (degree%360)<=180:
+        print("less than 180")
+        robot.motors(-1, 1, (degree)/58.8)
+    if degree%360>180:
+        robot.motors(1,-1, 180-(degree-180))
+    #og sensor reading is 260
+    left,right=robot.sonars()
+    print(left, right)
+    if left<=260 or right<=260:
+        robot.motors(-1,-1, 260-left)
+    if left>260 or right>260:
+        robot.motors(1,1, left-260)
+
 def stay():
+    global degree
     stay_in_place = float(input("How long do you want to stay for? "))
     robot.motors(0,0,stay_in_place)
+    degree = (degree)%360
     move()
 
 def move():
+    global degree
     hi = input("Hi! I'm robobunny. What would you like me to do? I can spin, turn90, dance(before you run this, make sure you are in the middle of the box), drive, or stay \n")
 
     if hi == str("spin"):
@@ -83,9 +118,15 @@ def move():
         drive()
     if hi == str("stay"):
         stay()
+    if hi == str("center"):
+        center()
     else:
         print("Sorry, I don't know how to do that. Please try again")
     move()
+
+# def move():
+#     spin("l", 1)
+#     center()
        
 move()
 
